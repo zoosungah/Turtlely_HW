218dcbda323042da22fe1b4f56d23f32279ef460
SungahChoo <blackdog03@naver.com>
2026-07-26 19:43:42 +0900

    feat: db 연결 수정

--- hardware_main.py
+++ hardware_main.py
@@ -46,11 +46,12 @@ class DailyReportSaveRequest(BaseModel):
 
 
 # 도커 MySQL 연결 설정
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "mysql+pymysql://0215@127.0.0.1:3306/turtlely_db"
-)
-# DATABASE_URL = "mysql+pymysql://root:choosungah03!@127.0.0.1:3306/turtlely_db"
+# DATABASE_URL = os.getenv(
+#    "DATABASE_URL", 
+#    "mysql+pymysql://0215@127.0.0.1:3306/turtlely_db"
+#)
+
+DATABASE_URL = "mysql+pymysql://root:choosungah03!@127.0.0.1:3306/turtlely_db"
 engine = create_engine(DATABASE_URL, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
